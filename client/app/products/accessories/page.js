d627646d27c8247d6e2a75445edf7251e2488480
Mootwar <romulo-132@hotmail.com>
2025-07-16 14:10:45 -0600

    Changed file structure to be compliant with NextJs and created first example of accessories page

---
+++ client/app/products/accessories/page.js
@@ -0,0 +1,28 @@
+import ProductCard from "@/app/components/ProductCard";
+
+export default function TShirtsPage () {
+    const products = [
+        {
+            id:1,
+            slug:"Classic-Tee",
+            title: "Classic Tee",
+            description: "A timeless cotton T-shirt.",
+            price: 25.0,
+            imgUrl: "/images/products/accessories/classicTee.jpg",
+        },
+        {
+            id:2,
+            slug: "v-neck-tee",
+            title: "V-Neck Tee",
+            description: "Soft V-Neck in multiple colors.",
+            price: 30.0,
+            imgUrl: "/images/products/accessories/vneck.jpg",
+        }];
+    return (
+        <div className="p-6 grid grd-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {products.map(p => (
+                <ProductCard key={p.id} product={p}/>
+            ))}
+        </div>
+    );
+}
