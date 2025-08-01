13525cc64f86ebcaba52010f055e740e3e466701
Mootwar <romulo-132@hotmail.com>
2025-08-01 10:26:57 -0600

    product card v2 finished

--- client/app/components/ProductCard.js
+++ client/app/components/ProductCard.js
@@ -7,29 +7,30 @@ export default function ProductCard ({ product }) {
     return (
         <Link
             href={`/product/${product.slug}`}
-            className="block overflow-hidden hover:shadow-lg transition-shadow duration-200 w-50">
-                <img
-                    src={product.imgUrl}
-                    alt={product.title}
-                    className="object-contain  "
-                />
-                <div className="p-4 sm:w-1/2 md:w-1/3 lg:w-1/4">
-                    <h2 className="text-lg font-semibold mb-2">{product.title}</h2>
-                    <p className="text-sm text-gray-600 mb-4">{product.description}</p>
-                    <div className="font-bold">
-                        <span>${product.price.toFixed(2)}</span>
-                        <button
-                            onClick={(e) => {
-                                e.preventDefault();
-                                //missing add card logic
-                            }}
-                            className="pl-2"
-                        >
-                            Add To Bag
-                        </button>
+            className="block w-full overflow-hidden hover:shadow-lg transition-shadow duration-200 rounded-lg p-4">
+                <div className="flex flex-col">
+                    <img
+                        src={product.imgUrl}
+                        alt={product.title}
+                        className="object-contain w-full h-48 mb-4"
+                    />
+                    <div className="w-full">
+                        <h2 className="text-lg font-semibold mb-2">{product.title}</h2>
+                        <p className="text-sm text-gray-600 mb-4">{product.description}</p>
+                        <div className="font-bold flex justify-between items-center">
+                            <span>${product.price.toFixed(2)}</span>
+                            <button
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    //missing add card logic
+                                }}
+                                className="bg-stone-950 text-white px-4 py-1 rounded-sm hover:animate-spin duration-200"
+                            >
+                                Add To Bag
+                            </button>
+                        </div>
                     </div>
                 </div>
-
         </Link>
     )
 }
