a2f8d81bc7384094a802b5ccfcfce42281b8912e
Mootwar <romulo-132@hotmail.com>
2025-08-01 10:29:44 -0600

    subfooter structure created content needs to be added

---
+++ client/app/components/SubFooter.js
@@ -0,0 +1,10 @@
+export default function SubFooter() {
+    return (
+        <section className="relative py-6 overflow-hidden">
+            <div className="absolute inset-0 bg-[url('/images/hero-bg.png')] bg-cover bg-center blur-sm"></div>
+            <div className="relative container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 px-4">
+                {/* Sub-footer content here */}
+            </div>
+        </section>
+    );
+}
